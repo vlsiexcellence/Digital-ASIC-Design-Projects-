c016249dfe86567009a479eb1b5dbdbe20496b17
VLSI Excellence - Gyan Chand Dhaka <119103983+vlsiexcellence@users.noreply.github.com>
2023-01-18 22:27:23 +0530

    Update event_detector_async.v

--- Digital Event Detector/HDL/event_detector_async.v
+++ Digital Event Detector/HDL/event_detector_async.v
@@ -22,16 +22,16 @@ module event_detector_async(
 		output    o_Event
     );
 
-  reg r_ff;   // Declare internal variables 
+reg r_ff;   // Declare internal variables 
 
-  always @(posedge clk or negedge reset_n) // Register the data_in
-		begin
+always @(posedge clk or negedge reset_n) // Register the i_Data
+	begin
           if(!reset_n)
-				r_ff <= 1'b0;
-			else
-				r_ff <= i_Data;
-			end
-  assign o_Event = (i_Data)^(r_ff); // event_out Generation
+		r_ff <= 1'b0;
+	  else
+		r_ff <= i_Data;
+	end
+ assign o_Event = (i_Data)^(r_ff); // o_Event Generation
 endmodule
 
 

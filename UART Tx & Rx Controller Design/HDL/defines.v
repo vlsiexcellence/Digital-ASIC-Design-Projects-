41b1e9c275fb750da4d68dbd9389d81cdfd56a4b
VLSI Excellence - Gyan Chand Dhaka <119103983+vlsiexcellence@users.noreply.github.com>
2023-01-20 14:42:41 +0530

    Create defines.v

---
+++ UART Tx & Rx Controller Design/HDL/defines.v
@@ -0,0 +1,11 @@
+        
+/*   
+
+Author : VLSI Excellence - Gyan Chand Dhaka (https://www.youtube.com/@vlsiexcellence)
+
+*/
+
+// Code your design here
+
+//`define UART_TX_ONLY 
+//`define UART_RX_ONLY 
